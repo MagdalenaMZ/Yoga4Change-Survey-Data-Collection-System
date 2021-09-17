710abcb15dc39b6a60380d92cff251d803e602cf
Magdalena Zarczynska <magdalenamzp@gmail.com>
2021-09-17 13:13:51 -0400

    Add project files.

---
+++ Yoga4Change Survey Data Collection System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Yoga4Change_Survey_Data_Collection_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
