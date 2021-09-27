575d7a3aa0ced759de68d6db18f7cd75ef9ed53a
Magdalena Zarczynska <magdalenamzp@gmail.com>
2021-09-27 16:25:06 -0400

    Added Question Management Controller

---
+++ Yoga4Change Survey Data Collection System/Controllers/QuestionMgmtController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Yoga4Change_Survey_Data_Collection_System.Models;
+
+namespace Yoga4Change_Survey_Data_Collection_System.Controllers
+{
+    public class QuestionMgmtController : Controller
+    {
+        private readonly ILogger<QuestionMgmtController> _logger;
+
+        public QuestionMgmtController(ILogger<QuestionMgmtController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
