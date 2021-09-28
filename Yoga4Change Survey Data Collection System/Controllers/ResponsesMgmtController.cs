991684fa48d538afa6c414347246e8371ab9eb5c
kuj-o <89951513+kuj-o@users.noreply.github.com>
2021-09-28 15:00:58 -0400

    added responses management controller

---
+++ Yoga4Change Survey Data Collection System/Controllers/ResponsesMgmtController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Yoga4Change_Survey_Data_Collection_System.Models;
+
+namespace Yoga4Change_Survey_Data_Collection_System.Controllers
+{
+    public class ResponsesMgmtController : Controller
+    {
+        private readonly ILogger<ResponsesMgmtController> _logger;
+
+        public ResponsesMgmtController(ILogger<ResponsesMgmtController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
