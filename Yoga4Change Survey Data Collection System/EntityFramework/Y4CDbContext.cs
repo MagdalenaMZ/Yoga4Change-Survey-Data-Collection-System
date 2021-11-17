fa93a5912410cbf60794752c633effd722efbd90
Magdalena Zarczynska <magdalenamzp@gmail.com>
2021-11-17 14:49:40 -0500

    Database

---
+++ Yoga4Change Survey Data Collection System/EntityFramework/Y4CDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yoga4Change_Survey_Data_Collection_System.EntityFramework
+{
+    public class Y4CDbContext : DbContext
+    {
+        public Y4CDbContext(DbContextOptions<Y4CDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+
+       /* public DbSet<Question> Questions
+        {
+
+        }
+       */
+    }
+}
