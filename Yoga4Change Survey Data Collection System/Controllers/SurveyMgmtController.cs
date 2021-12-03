547bbf9678d0de8d2eb2aa2d1314213d543c09cc
Ethan Elfersy <etelfersy@gmail.com>
2021-12-03 17:38:04 -0500

    Added placeholder survey list

--- Yoga4Change Survey Data Collection System/Controllers/SurveyMgmtController.cs
+++ Yoga4Change Survey Data Collection System/Controllers/SurveyMgmtController.cs
@@ -1,19 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Yoga4Change_Survey_Data_Collection_System.Models;
 
 namespace Yoga4Change_Survey_Data_Collection_System.Controllers
 {
     public class SurveyMgmtController : Controller
     {
+        [HttpPost]
         public ViewResult CreateSurvey()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ViewResult Surveys()
+        {
+            var s = new List<Survey>();
+            s.Add(new Survey { ID = 1, Name = "", Questions = new Survey.Question { ID = 1, Content = "", Type = "", Required = true }, Published = false });
+            s.Add(new Survey { ID = 2, Name = "", Questions = new Survey.Question { ID = 2, Content = "", Type = "", Required = true }, Published = false });
+            s.Add(new Survey { ID = 3, Name = "", Questions = new Survey.Question { ID = 3, Content = "", Type = "", Required = true }, Published = false });
+
+            return View(s);
         }
     }
 }
