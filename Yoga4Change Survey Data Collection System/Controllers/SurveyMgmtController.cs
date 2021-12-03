fcba058ac2827d9225d991596c77692eae2b3d2e
Ethan Elfersy <etelfersy@gmail.com>
2021-12-03 16:45:41 -0500

    Added basic survey functions

--- Yoga4Change Survey Data Collection System/Controllers/SurveyMgmtController.cs
+++ Yoga4Change Survey Data Collection System/Controllers/SurveyMgmtController.cs
@@ -11,27 +11,9 @@ namespace Yoga4Change_Survey_Data_Collection_System.Controllers
 {
     public class SurveyMgmtController : Controller
     {
-        private readonly ILogger<SurveyMgmtController> _logger;
-
-        public SurveyMgmtController(ILogger<SurveyMgmtController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
+        public ViewResult CreateSurvey()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
