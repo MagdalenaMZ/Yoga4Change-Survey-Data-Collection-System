f2c1f6aa6411e23c2f290fefb052be7d21fd1c99
Ethan Elfersy <etelfersy@gmail.com>
2021-12-10 16:20:57 -0500

    Changed surveys page to tabular format and added datatables

--- Yoga4Change Survey Data Collection System/Controllers/SurveyMgmtController.cs
+++ Yoga4Change Survey Data Collection System/Controllers/SurveyMgmtController.cs
@@ -16,9 +16,9 @@ namespace Yoga4Change_Survey_Data_Collection_System.Controllers
         public ViewResult Surveys()
         {
             var s = new List<Survey>();
-            s.Add(new Survey { ID = 1, Name = "", Questions = new Survey.Question { ID = 1, Content = "", Type = "", Required = true }, Published = false });
-            s.Add(new Survey { ID = 2, Name = "", Questions = new Survey.Question { ID = 2, Content = "", Type = "", Required = true }, Published = false });
-            s.Add(new Survey { ID = 3, Name = "", Questions = new Survey.Question { ID = 3, Content = "", Type = "", Required = true }, Published = false });
+            s.Add(new Survey { ID = 1, Name = "Survey1", Questions = new Survey.Question { ID = 1, Content = "...", Type = "date", Required = true }, Published = false });
+            s.Add(new Survey { ID = 2, Name = "Survey2", Questions = new Survey.Question { ID = 2, Content = "...", Type = "text", Required = true }, Published = false });
+            s.Add(new Survey { ID = 3, Name = "Survey3", Questions = new Survey.Question { ID = 3, Content = "...", Type = "text", Required = true }, Published = false });
 
             return View(s);
         }
