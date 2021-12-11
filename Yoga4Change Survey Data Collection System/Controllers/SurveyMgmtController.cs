654d2ea7f20d7d18e47cc515374892f313ad2d8a
Ethan Elfersy <etelfersy@gmail.com>
2021-12-10 19:41:41 -0500

    added mock survey data and updated navbar

--- Yoga4Change Survey Data Collection System/Controllers/SurveyMgmtController.cs
+++ Yoga4Change Survey Data Collection System/Controllers/SurveyMgmtController.cs
@@ -6,7 +6,6 @@ namespace Yoga4Change_Survey_Data_Collection_System.Controllers
 {
     public class SurveyMgmtController : Controller
     {
-        [HttpPost]
         public ViewResult CreateSurvey()
         {
             return View();
@@ -16,9 +15,16 @@ namespace Yoga4Change_Survey_Data_Collection_System.Controllers
         public ViewResult Surveys()
         {
             var s = new List<Survey>();
-            s.Add(new Survey { ID = 1, Name = "Survey1", Questions = new Survey.Question { ID = 1, Content = "...", Type = "date", Required = true }, Published = false });
-            s.Add(new Survey { ID = 2, Name = "Survey2", Questions = new Survey.Question { ID = 2, Content = "...", Type = "text", Required = true }, Published = false });
-            s.Add(new Survey { ID = 3, Name = "Survey3", Questions = new Survey.Question { ID = 3, Content = "...", Type = "text", Required = true }, Published = false });
+            s.Add(new Survey { ID = 1, Name = "Survey1", Questions = new Survey.Question { ID = 1, Content = "question1", Type = "date", Required = true }, Published = false });
+            s.Add(new Survey { ID = 2, Name = "Survey2", Questions = new Survey.Question { ID = 1, Content = "question1", Type = "open-ended", Required = true }, Published = false });
+            s.Add(new Survey { ID = 3, Name = "Survey3", Questions = new Survey.Question { ID = 1, Content = "question1", Type = "open-ended", Required = false }, Published = false });
+            s.Add(new Survey { ID = 4, Name = "Survey4", Questions = new Survey.Question { ID = 1, Content = "question1", Type = "open-ended", Required = false }, Published = false });
+            s.Add(new Survey { ID = 5, Name = "Survey5", Questions = new Survey.Question { ID = 1, Content = "question1", Type = "open-ended", Required = false }, Published = false });
+            s.Add(new Survey { ID = 6, Name = "Survey6", Questions = new Survey.Question { ID = 1, Content = "question1", Type = "open-ended", Required = false }, Published = false });
+            s.Add(new Survey { ID = 7, Name = "Survey7", Questions = new Survey.Question { ID = 1, Content = "question1", Type = "open-ended", Required = false }, Published = false });
+            s.Add(new Survey { ID = 8, Name = "Survey8", Questions = new Survey.Question { ID = 1, Content = "question1", Type = "open-ended", Required = false }, Published = false });
+            s.Add(new Survey { ID = 9, Name = "Survey9", Questions = new Survey.Question { ID = 1, Content = "question1", Type = "open-ended", Required = false }, Published = false });
+            s.Add(new Survey { ID = 10, Name = "Survey10", Questions = new Survey.Question { ID = 1, Content = "question1", Type = "open-ended", Required = false }, Published = false });
 
             return View(s);
         }
