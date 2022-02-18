d1847da3d5f4796aace063c9484ce7b7b44f1b41
etelfersy <etelfersy@gmail.com>
2022-02-18 13:04:00 -0500

    Added Survey table to DB project

---
+++ Y4C-DB/dbo/tables/Survey.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Survey]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL, 
+    [Description] VARCHAR(500) NOT NULL,
+    [IsActive] BIT NOT NULL,
+
+    CONSTRAINT [PK_Survey_Id] PRIMARY KEY ([Id])
+)
