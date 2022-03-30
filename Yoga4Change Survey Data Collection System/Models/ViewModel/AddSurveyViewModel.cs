3752c5bed9a4b432e69e3b8fe2235ad187436d4b
etelfersy <etelfersy@gmail.com>
2022-03-30 15:35:56 -0400

    Updated Create Survey page with datatables and more consistent question display, added a viewmodel for the Create page, and added javascript to help with selecting survey questions

---
+++ Yoga4Change Survey Data Collection System/Models/ViewModel/AddSurveyViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yoga4Change_Survey_Data_Collection_System.Models.ViewModel
+{
+    public class AddSurveyViewModel
+    {
+        public IEnumerable<Question> Questions { get; set; }
+        public Survey Survey { get; set; }
+    }
+}
