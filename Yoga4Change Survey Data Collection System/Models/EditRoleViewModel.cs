63f77393ef5db7a326d45a231a5c9da195633f5d
kuj-o <89951513+kuj-o@users.noreply.github.com>
2022-04-24 16:51:34 -0400

    Changes role name display

--- Yoga4Change Survey Data Collection System/Models/EditRoleViewModel.cs
+++ Yoga4Change Survey Data Collection System/Models/EditRoleViewModel.cs
@@ -13,6 +13,7 @@ namespace Yoga4Change_Survey_Data_Collection_System.Models
         public string Id { get; set; }
 
         [Required(ErrorMessage = "Role Name is required")]
+        [Display(Name = "Role Name")]
         public string RoleName { get; set; }
 
         public List<string> Users { get; set; }
