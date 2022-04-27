31a64d1994cc29c89867ddaacdd11962c7ffcb62
etelfersy <74470027+etelfersy@users.noreply.github.com>
2022-04-27 16:57:55 -0400

    Feature/survey management (#18)
    * Updated Create Survey page with datatables and more consistent question display, added a viewmodel for the Create page, and added javascript to help with selecting survey questions
    
    * Updated models for surveys

---
+++ Y4C-DB/dbo/tables/SurveyQuestions.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[SurveyId] INT NOT NULL,
+	[QuestionId] INT NOT NULL,
+	[OrderInSurvey] INT NOT NULL,
+	CONSTRAINT [PK_SurveyQuestions_Id] PRIMARY KEY ([Id]),
+	CONSTRAINT [FK_SurveyQuestions_SurveyId] FOREIGN KEY ([SurveyId]) REFERENCES [Survey]([Id]) ON DELETE CASCADE,
+	CONSTRAINT [FK_SurveyQuestions_QuestionId] FOREIGN KEY ([QuestionId]) REFERENCES [Questions]([Id]) ON DELETE CASCADE
+)
